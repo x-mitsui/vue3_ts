f394f388ccfeaf15a47c580ef180527ee83b000f
dzq <x_mitsui@163.com>
2022-09-11 13:05:34 +0800

    feat: 支持vue-router和vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <el-button type="success">I am ElButton</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
+// import { ElButton } from 'element-plus'
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
+  // components: { ElButton }
 })
 </script>
