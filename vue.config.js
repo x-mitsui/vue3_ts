e0aafc1a6a6dbbd79460b5b4338498257d3b9d4f
dzq <x_mitsui@163.com>
2022-09-11 13:07:41 +0800

    fix: element-plus自动导入功能

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,26 @@
-const path = require('path')
-
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = {
   // 1.配置方式一: CLI提供的属性
   // outputDir: './build',
   // publicPath: './',
   // 2.配置方式二: 和webpack属性完全一致, 最后会进行合并
+
   configureWebpack: {
     resolve: {
       alias: {
         components: '@/components' //"@"对应"src"已经在vue源码中配置了
       }
-    }
+    },
+    //配置webpack自动按需引入element-plus，
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
   }
 }
