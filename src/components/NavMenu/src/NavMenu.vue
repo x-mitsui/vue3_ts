d4486202571870ac2f45d1578b4a7ddc8beda42c
dzq <x_mitsui@163.com>
2022-09-16 15:40:43 +0800

    feat: 改名Home;增加NavMenu

---
+++ src/components/NavMenu/src/NavMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav-menu-wrapper">
+    <img src="~@/assets/img/logo.svg" alt="logo" class="logo" />
+    <h2 class="title">Vue3+TS</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.nav-menu-wrapper {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 30px;
+  padding: 5px;
+  .logo {
+    width: 30px;
+  }
+  .title {
+    color: white;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,19 @@
+import { LocalCache } from '@/utils/LocalCache'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
 
 const routes: Readonly<RouteRecordRaw[]> = [
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: '/',
+    redirect: '/main'
   },
   {
-    path: '/',
+    path: '/main',
+    name: 'Main',
+    component: Main
+  },
+  {
+    path: '/login',
     name: 'Login',
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/login/login.vue')
@@ -19,5 +24,16 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
+router.beforeEach((to) => {
+  if (to.path !== '/login') {
+    if (LocalCache.get('token')) {
+      return true
+    } else {
+      router.replace('/login')
+      return false
+    }
+  } else {
+    return true
+  }
+})
 export default router
