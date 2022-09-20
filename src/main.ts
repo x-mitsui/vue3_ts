76f89f279f6345bf51f7547e646cef2cd6c23223
dzq <x_mitsui@163.com>
2022-09-20 20:35:05 +0800

    feat: elementPlus国际化配置

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import { restoreLocalStorage, store } from './store'
 import 'normalize.css'
 import './assets/css/index.less'
 import { globalRegister } from './global'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 app.use(store)
+app.use(ElementPlus, {
+  locale: zhCn
+})
 
 restoreLocalStorage()
 app.use(router)
