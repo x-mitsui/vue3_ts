5897176bfe2d34cdeb4a6526b07b15c2c5f34745
dzq <x_mitsui@163.com>
2022-09-20 17:30:41 +0800

    feat: 集成dayjs库

---
+++ src/utils/dateFormat.ts
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+dayjs.extend(utc)
+const FORMAT_STRING = 'YYYY-MM-DD HH:mm:ss'
+
+const formatUTCString = (utcStr: string, formatStr: string = FORMAT_STRING) => {
+  console.log('utcStr:', utcStr)
+  return dayjs.utc(utcStr).local().format(formatStr)
+}
+const formatTimeStamp = (
+  timeStamp: number, //10位数字
+  formatStr: string = FORMAT_STRING
+) => {
+  return dayjs.unix(timeStamp).local().format(formatStr)
+}
+
+export { formatUTCString, formatTimeStamp }
